daf97d508cd8470574552c89146d23c3fd835729
Michelle Fogwell <mlf5035@gmail.com>
2019-06-27 15:23:06 -0400

    testscript
    added

---
+++ testscript.sh
@@ -0,0 +1,28 @@
+#test ci_committer_username 
+echo $CI_COMMITTER_USERNAME
+echo $CI_COMMITTER_NAME
+
+ls $HOME/cache
+echo '^ home cache'
+
+
+ls
+echo '^ current dir'
+
+
+mkdir -p codetest
+echo '^made codetest-'
+
+
+pwd
+ls
+echo '^pwd ls'
+
+cp -R codetest/ $HOME/cache/
+echo '^ copy files'
+
+pwd
+ls
+echo '^pwd and ls after copying'
+echo ''
+ls $HOME/cache
