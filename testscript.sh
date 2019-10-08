5a8fb4ff643d67db54f90844356cc90e563708c2
Michelle Fogwell <mlf5035@gmail.com>
2019-10-08 12:03:37 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,29 +1,27 @@
-#test ci_committer_username 
-echo $CI_COMMITTER_USERNAME
-echo $CI_COMMITTER_NAME
+#!/bin/bash
+# Install a custom FreeTDS version - http://www.freetds.org
+#
+# To run this script on Codeship, add the following
+# command to your project's setup commands:
+# \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/freetds.sh | bash -s
+#
+TDS_DIR=${TDS_DIR:=$HOME/cache/freetds}
 
-ls $HOME/cache
-echo '^ home cache'
+set -e
 
+if [ ! -d "${TDS_DIR}" ]; then
+  CACHED_DOWNLOAD="${HOME}/cache/freetds.tar.gz"
 
-ls
-echo '^ current dir'
+  mkdir -p "${HOME}/freetds"
+  wget --continue --output-document "${CACHED_DOWNLOAD}" "ftp://ftp.freetds.org/pub/freetds/stable/freetds-patched.tar.gz"
+  tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${HOME}/freetds"
 
+  (
+    cd "${HOME}/freetds" || exit 1
+    ./configure --prefix="${TDS_DIR}"
+    make
+    make install
+  )
+fi
 
-mkdir -p codetest
-echo '^made codetest-'
-
-
-pwd
-ls
-echo '^pwd ls'
-
-cp -R codetest/ $HOME/cache/
-echo '^ copy files'
-
-pwd
-ls
-echo '^pwd and ls after copying'
-echo ''
-ls $HOME/cache
-#test
+ln -s "${TDS_DIR}/bin/"* "${HOME}/bin"
